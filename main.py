408e0956f02c12520a3aaf878ccab723cb6bda67
s3ko3s <s3ko3s@gmail.com>
2023-01-10 21:09:52 +0300

    Camera_main

--- main.py
+++ main.py
@@ -111,6 +111,21 @@ def pause_screen():
         pygame.display.flip()
 
 
+
+# камера перемещения
+class Camera(object):
+    def __init__(self, camera_func, width, height):
+        self.camera_func = camera_func
+        self.state = Rect(0, 0, width, height)
+
+    def apply(self, target):
+        return target.rect.move(self.state.topleft)
+
+    def update(self, target):
+        self.state = self.camera_func(self.state, target.rect)
+
+
+
 # конфигурация камеры
 def camera_configure(camera, target_rect):
     l, t, _, _ = target_rect
